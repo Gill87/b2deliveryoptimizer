9021574f488ee92a52c1ca943d7b13b999cb2d57
Guritfak Gill <guritfak2007@gmail.com>
2026-05-24 13:32:35 -0700

    fix(edit): be explicit with aria-hidden

--- app/ui/src/app/edit/components/shared/OptimizeErrorPopup.tsx
+++ app/ui/src/app/edit/components/shared/OptimizeErrorPopup.tsx
@@ -59,7 +59,7 @@ export default function OptimizeErrorPopup({
               stroke="currentColor"
               strokeWidth="2"
               strokeLinecap="round"
-              aria-hidden
+              aria-hidden="true"
             >
               <line x1="18" y1="6" x2="6" y2="18" />
               <line x1="6" y1="6" x2="18" y2="18" />
